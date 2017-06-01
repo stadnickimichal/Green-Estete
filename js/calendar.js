5bc4ab3ae0a9253a2b8e8e30e5501486cb7576d1
tofik92d <stadnicki.michal@o2.pl>
2017-06-01 21:14:04 +0200

    commit #7

--- js/calendar.js
+++ js/calendar.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function(){
             this.td=$('td');
             this.input=$('input');
             this.select=$('select');
+            this.mouseCourse;
+            this.previouseMouseY=0;
         }
 
         begin(){
@@ -21,9 +23,10 @@ document.addEventListener("DOMContentLoaded", function(){
             this.select[1].value= min;
             this.td.each((index,element)=>{
                 $(element).unbind();
-                if ($(element).closest("table").attr('id')==day){
+                if ($(element).closest("table").attr('id')===day){
                     $(element).bind('click',()=>this.function2(element));
                     $(element).bind('mouseout',()=>this.function3(element));
+                    $(element).bind('mousemove',()=>this.mouseMove(event));
                 };
             });
         }
@@ -31,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function(){
             let tdIndex= element.id.split("-")[1];
             let min= (tdIndex%4)*15;
             let h= Math.floor(tdIndex/4)+6;
+            $(element).css("background-color",'rgb(0, 128, 0)');
             this.input[2].value= h;
             this.select[2].value= min;
             this.td.each((index,element)=>{
@@ -38,9 +42,24 @@ document.addEventListener("DOMContentLoaded", function(){
             });
         }
         function3(element){
-            console.log($(element).css("background-color"));
-            console.log(($(element).css("background-color")=='rgba(0, 0, 0, 0)')?'rgb(0, 128, 0)':'rgba(0, 0, 0, 0)');
-            $(element).css("background-color", ($(element).css("background-color")=='rgba(0, 0, 0, 0)')?'rgb(0, 128, 0)':'rgba(0, 0, 0, 0)');
+            if(this.mouseCourse){
+                $(element).css("background-color",'rgb(0, 128, 0)');
+                $(element).css("border",'0');
+            }
+            else{
+                $(element).css("background-color",'rgba(0, 0, 0, 0)');
+                $(element).css("border",'1px solid #000000');
+            }
+        }
+        mouseMove(event){
+            if(this.previouseMouseY<event.pageY){
+                this.mouseCourse=true;
+            }
+            else{
+                this.mouseCourse=false;
+            }
+            this.previouseMouseY=event.pageY;
+            console.log(this.mouseCourse);
         }
     }
     let obj1 = new addingData();
